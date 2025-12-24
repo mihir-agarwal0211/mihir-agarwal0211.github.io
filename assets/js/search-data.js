de4b2ac6fc451a5d3976fea9c769606fec54539b
mihir-agarwal0211 <mihiragarwal1999@gmail.com>
2025-12-24 04:45:28 +0000

    Deploying to gh-pages from @ mihir-agarwal0211/mihir-agarwal0211.github.io@00731d0cf22081a9d6e4cdecba4579c12e16d3aa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -456,46 +456,16 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-music-transformer",
+          title: 'Music Transformer',
+          description: "Comparing Vanilla Attention vs. Relative Attention for MIDI generation.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/music_transformer/";
+            },},{id: "projects-neurolob",
+          title: 'NeuroLOB',
+          description: "Generative Market Intelligence using Neural Point Processes &amp; Diffusion.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/neurolob/";
             },},{
         id: 'social-cv',
         title: 'CV',
